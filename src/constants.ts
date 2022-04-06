9bd6169f94f7b6de1cd43f7047302287dc13812d
Salah-BOUYAHIA <salah.bouyahia-ext@aphp.fr>
2022-04-06 19:15:51 +0200

    wip

--- src/constants.ts
+++ src/constants.ts
@@ -25,7 +25,8 @@ export const TOKEN_URL = 'irrelevant'
 export const REVOKE_URL = 'irrelevant'
 export const LOGIN_REDIRECT_URL = 'irrelevant'
 
-export const CONTEXT = process.env.REACT_APP_CONTEXT as ContextType
+export const CONTEXT =
+  process.env.NODE_ENV !== 'development' ? '{CONTEXT}' : (process.env.REACT_APP_CONTEXT as ContextType)
 
 if (!CONTEXT) throw new Error('missing REACT_APP_CONTEXT from environment')
 if (CONTEXT !== 'arkhn' && CONTEXT !== 'aphp' && CONTEXT !== 'fakedata') {
