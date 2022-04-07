7ed6a03793689795ccba96222279ec858423d0ec
Salah-BOUYAHIA <salah.bouyahia-ext@aphp.fr>
2022-04-07 11:15:40 +0200

    WIP: debug for gitlab CI

--- src/constants.ts
+++ src/constants.ts
@@ -28,12 +28,16 @@ export const LOGIN_REDIRECT_URL = 'irrelevant'
 export const CONTEXT =
   process.env.NODE_ENV !== 'development' ? '{CONTEXT}' : (process.env.REACT_APP_CONTEXT as ContextType)
 
-console.log('CONTEXT', CONTEXT)
+console.log('CONTEXT before', CONTEXT)
+console.log('process', process.env)
 
 if (!CONTEXT) throw new Error('missing REACT_APP_CONTEXT from environment')
 if (CONTEXT !== 'arkhn' && CONTEXT !== 'aphp' && CONTEXT !== 'fakedata') {
   throw new Error("REACT_APP_CONTEXT must be either 'aphp', 'fakedata' or 'arkhn")
 }
+
+console.log('CONTEXT AFTER', CONTEXT)
+
 export const ACCES_TOKEN = 'access'
 export const REFRESH_TOKEN = 'refresh'
 
